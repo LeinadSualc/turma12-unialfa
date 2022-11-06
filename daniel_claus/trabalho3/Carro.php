8c6f73d734845c992288191ecbaee9ce013fd00a
Daniel Claus <danclaus2002@gmail.com>
2022-11-06 04:08:22 -0300

    trabalho 3

---
+++ daniel_claus/trabalho3/Carro.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Unialfa\trabalho3
+
+
+class Carro{
+
+    public string $marca;
+    public array $detalhes = ["cor", "farol", "portas", "pneus"];
+    public int $tempoFabricacao;
+
+
+}
+
